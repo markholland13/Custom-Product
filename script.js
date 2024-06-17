951ad2aebe88852d96c7f5be6dacb2cc8c26351c
markholland13 <mark@thg.co>
2024-06-17 15:59:04 +0100

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Handle iframe navigation
+window.addEventListener('message', function(event) {
+    if (event.data.action === 'navigate') {
+        const iframe = document.getElementById('product-iframe');
+        iframe.src = event.data.page;
+        setTimeout(() => {
+            iframe.contentWindow.postMessage({
+                action: 'loadProduct',
+                product: event.data.product
+            }, '*');
+        }, 500);
+    }
+});
